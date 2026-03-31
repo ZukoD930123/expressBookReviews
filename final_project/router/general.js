6e9e471c02314f9b64f8ccb349af78a1dde47eab
ZukoD930123 <lindodondolo@gmail.com>
2026-03-31 10:37:16 +0200

    Completed Task 12 using Async-Await for Author search

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -68,30 +68,34 @@ public_users.get('/isbn/:isbn', function (req, res) {
       res.status(404).json({ message: err });
     });
 });
- 
-// Get book details based on author
-public_users.get('/author/:author', function (req, res) {
-  const author = req.params.author;
-  const keys = Object.keys(books); // Get all ISBNs
-  const results = [];
 
-  keys.forEach((key) => {
-    if (books[key].author === author) {
-      results.push({
-        "isbn": key,
-        "title": books[key].title,
-        "reviews": books[key].reviews
-      });
+// Task 12: Get book details based on Author using async-await
+public_users.get('/author/:author', async function (req, res) {
+    const author = req.params.author;
+    try {
+        const getBooksByAuthor = new Promise((resolve, reject) => {
+            const keys = Object.keys(books);
+            const filtered_books = keys
+                .filter(key => books[key].author === author)
+                .map(key => ({
+                    isbn: key,
+                    title: books[key].title,
+                    reviews: books[key].reviews
+                }));
+            
+            if (filtered_books.length > 0) {
+                resolve(filtered_books);
+            } else {
+                reject("Author not found");
+            }
+        });
+
+        const results = await getBooksByAuthor;
+        return res.status(200).send(JSON.stringify({ booksbyauthor: results }, null, 4));
+    } catch (error) {
+        return res.status(404).json({ message: error });
     }
-  });
-
-  if (results.length > 0) {
-    return res.status(200).send(JSON.stringify({ booksbyauthor: results }, null, 4));
-  } else {
-    return res.status(404).json({ message: "Author not found" });
-  }
 });
-
 
 // Get book details based on title
 public_users.get('/title/:title', function (req, res) {
